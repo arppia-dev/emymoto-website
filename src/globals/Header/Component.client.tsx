abc1b4a7e606bcd77b9ecf603e5a00e80628768a
Alexis Sniffer <alexis.sniffer@gmail.com>
2025-04-05 13:01:03 -0500

    se añaden componentes de pagina de payload

---
+++ src/globals/Header/Component.client.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import type { Header } from '@/payload-types'
+import Link from 'next/link'
+import React from 'react'
+import { HeaderNav } from './Nav'
+
+interface HeaderClientProps {
+  data: Header
+}
+
+export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
+  return (
+    <header className="bg-sky-950">
+      <div className="container mx-auto flex items-center justify-between px-4 py-2">
+        <Link href="/"></Link>
+        <div className="flex items-center gap-4">
+          <HeaderNav data={data} />
+          <button className="z-10 hidden bg-sky-800 px-6 py-4 font-semibold text-white hover:bg-amber-200 hover:text-sky-950 md:block">
+            Obten una cita
+          </button>
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ src/globals/Header/Component.tsx
@@ -0,0 +1,17 @@
+import { HeaderClient } from './Component.client'
+import { type Header } from '@/payload-types'
+
+async function getHeaderData() {
+  const res = await fetch(`${process.env.PAYLOAD_URL}/api/globals/header`, {
+    cache: 'no-store',
+  })
+
+  if (!res.ok) throw new Error('Failed to fetch header data')
+
+  return res.json()
+}
+
+export async function Header() {
+  const headerData = await getHeaderData()
+  return <HeaderClient data={headerData} />
+}
