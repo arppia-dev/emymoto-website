4f8772a460d25847936d22d8c5b9ba9c1f12bd4f
Alexis Sniffer <alexis.sniffer@gmail.com>
2025-05-31 19:59:09 -0500

    print env

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -24,6 +24,19 @@ const serverFunction: ServerFunctionClient = async function (args) {
 
 const Layout = ({ children }: Args) => (
   <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
+    <pre>
+      {JSON.stringify(
+        {
+          S3_BUCKET: process.env.S3_BUCKET,
+          S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+          S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+          S3_ENDPOINT: process.env.S3_ENDPOINT,
+          S3_REGION: process.env.S3_REGION,
+        },
+        null,
+        2,
+      )}
+    </pre>
     {children}
   </RootLayout>
 )
