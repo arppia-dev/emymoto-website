0849c8c2c6880fca62ed98f4a034565304ffb083
Alexis Sniffer <alexis.sniffer@gmail.com>
2025-06-05 22:52:33 -0500

    correccion en header

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -31,7 +31,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
 
   return (
     <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
-      <div className="py-8 flex justify-between">
+      <div className="pb-8 flex  items-center md:align-middle md:justify-between flex-col md:flex-row ">
         <Link href="/">
           <Logo loading="eager" priority="high" className="invert dark:invert-0" />
         </Link>
